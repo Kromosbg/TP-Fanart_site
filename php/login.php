2b3f65d96025cbd5a99d19fa609a458f37dd47a8
Kromosbg <ivo383@abv.bg>
2014-04-17 00:09:04 +0300

    Create login.php

---
+++ php/login.php
@@ -0,0 +1,10 @@
+<body>
+
+<form action="script.php" method="post">
+Name: <input type="text" name="name"><br>
+Password: <input type="text" name="Password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
