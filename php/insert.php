8d12ed1ac10da6f6eaba0d15b61eff6d73bf0817
Kromosbg <ivo383@abv.bg>
2014-04-16 23:45:38 +0300

    Create insert.php

---
+++ php/insert.php
@@ -0,0 +1,29 @@
+<? php
+$con=mysqli_connect('localhost','root', '235101Bg',  "my_db");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+// escape variables for security
+$Username= mysqli_real_escape_string($_POST['Username']);
+$Password = mysqli_real_escape_string($_POST['Password']);
+$Email = mysqli_real_escape_string($_POST['Email']);
+
+
+$sql="INSERT INTO Register (Username, Password, Email)
+VALUES ($Username, $Password, $Email)";
+
+if (!mysqli_query($con,$sql))
+{
+  die('Error: ' . mysqli_error($con));
+}
+echo "1 record added";
+mysqli_query($con,"UPDATE Register SET Username
+WHERE ID=1");
+
+
+mysqli_close($con);
+?> 
+ 
