c5a4076aa4d09b02f271a3d1b9098fc75ce0f48e
Kromosbg <ivo383@abv.bg>
2014-05-27 12:23:51 +0300

    Update insert.php

--- php/insert.php
+++ php/insert.php
@@ -1,34 +1,33 @@
+
+  
+<html>
+<body>
+<head>
+<style>
+body
+{
+background-color:#D4EEF7;;
+}
+</style>
+</head>
 <? php
 $con=mysqli_connect('localhost','root', '235101Bg',  "my_db");
 
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  
-// escape variables for security
-$Username= mysqli_real_escape_string($_POST['Username']);
-$Password = mysqli_real_escape_string($_POST['Password']);
-$Email = mysqli_real_escape_string($_POST['Email']);
 
-
-$sql="INSERT INTO Register (Username, Password, Email)
-VALUES ($Username, $Password, $Email)";
+$sql="INSERT INTO User(Username, Password, Email)
+VALUES ('$Username', '$Password', '$Email')";
 
 if (!mysqli_query($con,$sql))
 {
   die('Error: ' . mysqli_error($con));
 }
 echo "1 record added";
-mysqli_query($con,"UPDATE Register SET Username
-WHERE ID=1");
 
+header("Location: http://www.example.com/"); /* Redirect browser */
 
-mysqli_close($con);
+/* Make sure that code below does not get executed when we redirect. */
+exit;
 ?> 
-  
- <html>
-<body>
 
 Welcome <?php echo $_POST["Username"]; ?><br>
 Your email address is: <?php echo $_POST["Email"]; ?>
