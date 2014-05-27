825c5ad4ca9ce7e15ebf8610ef815216e78726ff
Kromosbg <ivo383@abv.bg>
2014-05-27 12:22:40 +0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,11 +1,53 @@
+ <?php 
+
+ // Connects to your Database 
+
+$con=mysqli_connect('localhost','root', '235101Bg');
+
+?>
+
+
 <html>
 <body>
+<head>
+<style>
+body
+{
+background-color:#D4EEF7;;
+}
+</style>
+</head>
 
+
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
 <form action="succes.php" method="post">
-Name: <input type="text" name="name"><br>
-Password: <input type="password" name="Password"><br>
-<input type="submit">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3"><strong>Member Login </strong></td>
+</tr>
+<tr>
+<td width="78">Username</td>
+<td width="6">:</td>
+<td width="294"><input type="text" name="name"><br>
+</tr>
+<tr>
+<td>Password</td>
+<td>:</td>
+<td><input type="password" name="Password"><br>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
 </form>
+</tr>
+</table>
+
 
 </body>
 </html>
